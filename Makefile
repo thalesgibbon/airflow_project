b231b56294fa75fcf97b003853806bf8f586a71a
thalesgibbon <hi@thalesgibbon.com>
2023-12-27 10:27:33 -0300

    example dag

--- Makefile
+++ Makefile
@@ -3,12 +3,16 @@ create-user:
 	airflow users create --username airflow --password airflow --firstname airflow --lastname airflow --role Admin --email seu_email@example.com
 
 run-server:
+	make set-airflow-project
+	echo checking AIRFLOW_HOME=$AIRFLOW_HOME
 	airflow webserver -p 8081
 
 print-server:
 	echo http://localhost:8081/home
 
 run-schedule:
+	make set-airflow-project
+	echo checking AIRFLOW_HOME=$AIRFLOW_HOME
 	airflow scheduler
 
 set-airflow-project:
@@ -16,6 +20,23 @@ set-airflow-project:
 
 airflow-init:
 	airflow db init
+
+airflow-check-dags:
+	airflow dags list
+
+clean-trash-airflow:
+	rm -r ~/airflow/
+
+check-port:
+	sudo lsof -t -i :8081 | xargs sudo kill -9
+
+resync:
+	make set-airflow-project
+	echo checking AIRFLOW_HOME=$AIRFLOW_HOME
+	airflow dags resync
+
+deploy:
+	cp -r ./dev/dags/ ./airflow/dags/
 
 setup:
 	make set-airflow-project
--- dev/dags/minha_dag.py
+++ dev/dags/minha_dag.py
@@ -1,15 +1,35 @@
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from datetime import datetime
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+
+
+class JinjaTemplates:
+    def __init__(self):
+        self.run_id = '{{ run_id }}'
+
+    def get_template(self, name):
+        return getattr(self, name, None)
+templates = JinjaTemplates()
 
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2023, 1, 1),
 }
+dag = DAG(
+    'minha_dag_com_jinja',
+    default_args=default_args,
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 1, 1),
+)
 
-dag = DAG('minha_dag', default_args=default_args, schedule_interval='@daily')
 
-t1 = DummyOperator(task_id='tarefa_1', dag=dag)
-t2 = DummyOperator(task_id='tarefa_2', dag=dag)
+def print_run_id(run_id):
+    print(f"Run ID: {run_id}")
 
-t1 >> t2
+
+tarefa_print_run_id = PythonOperator(
+    task_id='tarefa_print_run_id',
+    python_callable=print_run_id,
+    op_kwargs={'run_id': templates.run_id},
+    dag=dag,
+)
